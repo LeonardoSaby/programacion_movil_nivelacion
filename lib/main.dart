ed81e99110bc4cb46eb639c1850090a272ffda3a
Leonardo <felixleonardosabysuarez16@gmail.com>
2025-08-05 19:41:09 -0400

    implementacion de los screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:programacion_movil_nivelacion/screens/splash_screen.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nivelacion Programacion',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: SplashScreen(),
+    );
   }
 }
-
