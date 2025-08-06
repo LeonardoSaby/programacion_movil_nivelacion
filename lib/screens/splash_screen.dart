3cabd194ea57e6f66de81ed971863d57c876de79
Leonardo <felixleonardosabysuarez16@gmail.com>
2025-08-05 20:34:40 -0400

    integracion del archivo onboarding_screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:programacion_movil_nivelacion/screens/onboarding_screen.dart';
 
 
 class SplashScreen extends StatefulWidget {
@@ -11,6 +14,45 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage("images/oso.png"),
+            fit: BoxFit.cover,
+            opacity: 0.4,
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.shopping_cart,
+            size: 250,
+            color: Colors.white70,),
+            Text(
+              "Coca Cola",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize:40,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic
+              ),)
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  void initState(){
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+        () => Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => OnboardingScreen()),
+    ));
   }
 }
